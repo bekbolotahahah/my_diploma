b3e9a1921ae191f55a26fcbc8c8d70d8d1dece61
bekbolotahahah <112771389+bekbolotahahah@users.noreply.github.com>
2022-12-07 00:20:01 +0600

    fi

--- src/features/counter/index.js
+++ src/features/counter/index.js
@@ -7,8 +7,8 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from './slice';
+import styles from './module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
